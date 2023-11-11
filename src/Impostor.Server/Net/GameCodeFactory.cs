964fb7200dd30985fed2e8db71cdc2c6ce7157c0
Bilibili_MengChu <2133404320@qq.com>
2023-11-12 01:09:10 +0800

    Revert "1"
    This reverts commit c095a2cecc67fc54e3c7e44c849c9083541d58f6.

--- src/Impostor.Server/Net/GameCodeFactory.cs
+++ src/Impostor.Server/Net/GameCodeFactory.cs
@@ -1,53 +1,12 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-using Impostor.Api.Games;
-using Impostor.Api.Games.Managers;
+﻿using Impostor.Api.Games;
 
 namespace Impostor.Server.Net
 {
-    public partial class GameCodeFactory : IGameCodeFactory
+    public class GameCodeFactory : IGameCodeFactory
     {
-        private readonly HashSet<string> _gameCodes = new();
-        private readonly IGameManager _gameManager;
-
-        public GameCodeFactory(IGameManager gameManager)
-        {
-            _gameManager = gameManager;
-            var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
-            var codeNameFile = directory.GetFiles().FirstOrDefault(n => n.Name == "CodeName.txt");
-            if (codeNameFile == null)
-            {
-                return;
-            }
-
-            var streamReader = new StreamReader(codeNameFile.FullName);
-            while (streamReader.ReadLine() is { } code)
-            {
-                var regex = MyRegex();
-                if (!regex.IsMatch(code))
-                {
-                    continue;
-                }
-
-                _gameCodes.Add(code);
-            }
-        }
-
         public GameCode Create()
         {
-            var codes = _gameCodes.Where(FindCode).ToList();
-            return codes.Count == 0 ? GameCode.Create() : new GameCode(codes.First());
+            return GameCode.Create();
         }
-
-        private bool FindCode(string code)
-        {
-            var games = _gameManager.Games.Select(n => n.Code.Code);
-            return !games.Contains(code);
-        }
-
-        [GeneratedRegex("[A-Z]{6}")]
-        private static partial Regex MyRegex();
     }
 }
